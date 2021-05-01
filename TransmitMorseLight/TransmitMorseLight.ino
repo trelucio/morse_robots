c9204ced6ee192dbbd6fb42f460305bb135d87fe
Therese Relucio <63369100+trelucio@users.noreply.github.com>
2021-05-01 00:31:08 -0500

    Working transmit morse light code

--- TransmitMorseLight/TransmitMorseLight.ino
+++ TransmitMorseLight/TransmitMorseLight.ino
@@ -18,8 +18,43 @@
   Arduino language reference can be found at http://arduino.cc/en/Reference/HomePage  
   Windows: USE SERIAL MONITOR -> Ctrl+Shift+M
   Mac:     USE SERIAL MONITOR -> Cmd+Shift+M
+
+  ---------------------------------------------------------------------------------
+  TRANSMIT MORSE LIGHT WORKING CODE BY THERESE RELUCIO
+  Networking, Databases, and Cryptograph: Spring 2021
+  Professor: Dr. Hochberg
+
+  1a. A string is encoded in SETUP() and uploaded to Color Beacon Robot
+  1b. To loop light message, use LOOP() and comment out SETUP() body
+  2. Encode() function reads string and converts every letter to morse code equivalent of . and -
+  3. Each symbol combination (e.g. ".-") is passed to morse2light() function
+  4a. Morse2light() converts every "." to a RED light and "-" to a BLUE light
+  4b. If multiple symbols are part of the same letter, WHITE will flash (represented by "/" in DecodeMorseLightWorking.ino)
+  4c. To separate letters, GREEN will flash (represented by "+" in DecodeMorseLightWorking.ino)
+  5. Color beacon transmits lights every 200 ms
+
+  COLOR  || SYMBOL || MEANING
+  RED    ||   .    || Equivalent of "dot in Morse Code
+  BLUE   ||   -    || Equivalent of "dash" in Morse Code  
+  WHITE  ||   /    || Separates individual morse symbols of same letter (e.g., ./- => .- => A)
+  GREEN  ||   +    || Separates individual letters/characters in English string
+  YELLOW ||   &    || End of Message token
+
+  * Current version does not work with spaces in message...yet.
+  * Link to Morse Code Alphabet: https://www.codebug.org.uk/learn/step/540/morse-code-alphabet/
+
+  SAMPLE MESSAGE: "MORSE"
+  Message as morse code string: -- --- .-. ... . (with no spaces)
+  Message that will show up in DecodeMorseLightWorking (no repeats): -/- + -/-/- + ./-/. + ././. + . &(with no spaces)
+  Message in Color Form: blue white blue green
+                         blue white blue white blue green
+                         red white blue white red green
+                         red white red white red green
+                         red yellow
 -------------------------------------------------------------------------------------------*/
 #include "CORE.h"
+
+/* String message -> char to morse -> morse to light */
 
 // Create an instance of class CORE_TRICOLOR named beacon on port S3
 CORE_COLOR_BEACON beacon;
@@ -27,29 +62,23 @@ CORE_COLOR_BEACON beacon;
 // Create an instance of the class CORE_DIGITAL named push on port D7
 CORE_DIGITAL push(7);
 
-// Run Morse Code Message ONCE: click reset to run again
+// Run Morse Code Message ONCE: click reset button on board to run again
 void setup(){
   beacon.setColor(beacon.OFF);
 
-  String msg1 = "MORSE"; 
-  String msg2 = "APPLE PIE";
-  String msg3 = "ROBOTS ARE COOL";
+  String msg = "MORSE";
+  String msg1 = "ARDUINO"; 
+  String msg2 = "HAVE A GOOD SUMMER";
 
-  encode(msg1);
+  encode(msg);
 }
 
-const int ms = 200; //delay time/light speed in ms; == interval
+// LIGHT INTERVAL: delay time/light speed in ms; == interval
+const int ms = 300;
 
-/* 
-* MORSE TO LIGHT CODE KEY
-* . = RED, - = BLUE
-* next letter/char = GREEN, space = WHITE
-* END OF MESSAGE = 3 long white flashes
-*/
-
-// MORSE TO LIGHT FUNCTION
-// s = morse string of letter received from encode function
-// returns light flashes on color beacon
+/* MORSE TO LIGHT FUNCTION
+   s = morse string of letter received from encode() function
+   returns light flashes on color beacon */
 void morse2light(String s){
   for (int i = 0; i < s.length(); i++){
     if (s.charAt(i) == '.'){
@@ -60,143 +89,122 @@ void morse2light(String s){
       beacon.setColor(beacon.BLUE);
       delay(ms);
     }
-    if (s.charAt(i) == ' '){
+
+    if (s.length() > 1 && i < s.length() - 1){
       beacon.setColor(beacon.WHITE);
       delay(ms);
     }
-    beacon.setColor(beacon.OFF);
-    delay(ms);
   }
-  // NEXT CHAR: flash green
-  beacon.setColor(beacon.GREEN);
-  delay(ms);    
-  beacon.setColor(beacon.OFF);
-  delay(ms);
 }
 
-// ENCODE FUNCTION
-// Translate message to string representations of morse code
-// Morse string given to morse2light function
+/* ENCODE FUNCTION
+   Translate message to string representations of morse code
+   Morse string given to morse2light function */
 void encode(String msg){
   for (int i = 0; i < msg.length(); i++){
-      switch(msg.charAt(i)){
-        case 'A':
-            morse2light(".-");
-            break;
-        case 'B':
-            morse2light("-...");
-            break;
-        case 'C':
-            morse2light("-.-.");
-            break;
-        case 'D':
-            morse2light("-..");
-            break;
-        case 'E':
-            morse2light(".");
-            break;
-        case 'F':
-            morse2light("..-.");
-            break;
-        case 'G':
-            morse2light("--.");
-            break;
-        case 'H':
-            morse2light("....");
-            break;
-        case 'I':
-            morse2light("..");
-            break;
-        case 'J':
-            morse2light(".---");
-            break;
-        case 'K':
-            morse2light("-.-");
-            break;
-        case 'L':
-            morse2light(".-..");
-            break;
-        case 'M':
-            morse2light("--");
-            break;
-        case 'N':
-            morse2light("-.");
-            break;
-        case 'O':
-            morse2light("---");
-            break;
-        case 'P':
-            morse2light(".--.");
-            break;
-        case 'Q':
-            morse2light("--.-");
-            break;
-        case 'R':
-            morse2light(".-.");
-            break;
-        case 'S':
-            morse2light("...");
-            break;
-        case 'T':
-            morse2light("-");
-            break;
-        case 'U':
-            morse2light("..-");
-            break;
-        case 'V':
-            morse2light("...-");
-            break;
-        case 'W':
-            morse2light(".--");
-            break;
-        case 'X':
-            morse2light("-..-");
-            break;
-        case 'Y':
-            morse2light("-.--");
-            break;
-        case 'Z':
-            morse2light("--..");
-            break;
-        }
-  }
+    switch(msg.charAt(i)){
+      case 'A':
+          morse2light(".-");
+          break;
+      case 'B':
+        morse2light("-...");
+        break;
+      case 'C':
+        morse2light("-.-.");
+        break;
+      case 'D':
+        morse2light("-..");
+        break;
+      case 'E':
+        morse2light(".");
+        break;
+      case 'F':
+        morse2light("..-.");
+        break;
+      case 'G':
+        morse2light("--.");
+        break;
+      case 'H':
+        morse2light("....");
+        break;
+      case 'I':
+        morse2light("..");
+        break;
+      case 'J':
+        morse2light(".---");
+        break;
+      case 'K':
+        morse2light("-.-");
+        break;
+      case 'L':
+        morse2light(".-..");
+        break;
+      case 'M':
+        morse2light("--");
+        break;
+      case 'N':
+        morse2light("-.");
+        break;
+      case 'O':
+        morse2light("---");
+        break;
+      case 'P':
+        morse2light(".--.");
+        break;
+      case 'Q':
+        morse2light("--.-");
+        break;
+      case 'R':
+        morse2light(".-.");
+        break;
+      case 'S':
+        morse2light("...");
+        break;
+      case 'T':
+        morse2light("-");
+        break;
+      case 'U':
+        morse2light("..-");
+        break;
+      case 'V':
+        morse2light("...-");
+        break;
+      case 'W':
+        morse2light(".--");
+        break;
+      case 'X':
+        morse2light("-..-");
+        break;
+      case 'Y':
+        morse2light("-.--");
+        break;
+      case 'Z':
+        morse2light("--..");
+        break;
+      }
 
-  // END OF MESSAGE: 3 Blinks of GREEN
-  beacon.setColor(beacon.GREEN);
+    // flash green for next letter if letter isn't the last in string
+    if (i < msg.length() - 1){
+      beacon.setColor(beacon.GREEN);
+      delay(ms); 
+    } 
+  }
+   
+  // END OF MESSAGE: YELLOW
+  beacon.setColor(beacon.YELLOW);
   delay(ms);
   beacon.setColor(beacon.OFF);
-  delay(ms);
-  beacon.setColor(beacon.GREEN);
-  delay(ms);
-  beacon.setColor(beacon.OFF);
-  delay(ms);
-  beacon.setColor(beacon.GREEN);
-  delay(ms);
-  beacon.setColor(beacon.OFF);
-  delay(ms);
 }
 
-/* 
-* MORSE2LIGHT CODE KEY
-* . = RED, - = BLUE
-* next letter/char = GREEN, space = WHITE
-* SAMPLE MESSAGE: "MORSE"
-* Message as morse code string: -- --- .-. ... . (with no spaces)
-*  lights should flash: blue blue green 
-*                       blue blue blue green 
-*                       red blue red green 
-*                       red red red green
-*                       red green
-*                       green green green 
-*/
 
-// LOOP()/ MAIN FUNCTION TO RUN ON ROBOT
+// LOOP() TO RUN ON ROBOT
 // Enter string, encode, and transmit through color beacon
 void loop(){
   /*beacon.setColor(beacon.OFF);
 
-  String message = "MORSE"; 
-  String message2 = "COMPUTER SCIENCE";
+  String msg = "MORSE";
+  String msg1 = "ARDUINO"; 
+  String msg2 = "HAVE A GOOD SUMMER";
 
-  encode(message);*/
+  encode(msg);*/
 }
